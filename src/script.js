ce552b0d98cfd07ab0105c3b0d354430cb0dcaed
Morwasi <140886703+Morwasi@users.noreply.github.com>
2024-04-10 23:58:03 +0200

    here

--- src/script.js
+++ src/script.js
@@ -2,33 +2,39 @@ document.addEventListener('DOMContentLoaded', () => {
     const loadCarsBtn = document.getElementById('loadCarsBtn');
     const carList = document.getElementById('carList');
     cars = [];
-    loadCarsBtn.addEventListener('click', () => {
-        fetch('http://localhost:3001/cars')
-            .then(response => response.json())
-            .then(data => {
-                cars = data;
-                carList.innerHTML = '';
-                data.forEach((car, index) => {
-                    const carCard = document.createElement('div');
-                    carCard.classList.add('car-card');
-                    carCard.innerHTML = `
-                        <h2>${car.make} ${car.model}</h2>
-                        <p><strong>Year:</strong> ${car.year}</p>
-                        <p><strong>Make:</strong> ${car.make}</p>
-                        <p><strong>Model:</strong> ${car.model}</p>
-                        <p><strong>Price:</strong> R${car.price}</p>
-                        <button class="btn btn-remove" data-index="${index}">Remove</button>
-                    `;
-                    carList.appendChild(carCard);
-                });
-            })
-            .catch(error => {
-                console.error('Error fetching car data:', error);
+
+    function loadCars(){
+        fetch('/api/cars')
+        .then(response => response.json())
+        .then(data => {
+            cars = data;
+            carList.innerHTML = '';
+            data.forEach((car, index) => {
+                const carCard = document.createElement('div');
+                carCard.classList.add('car-card');
+                carCard.innerHTML = `
+                    <h2>${car.make} ${car.model}</h2>
+                    <p><strong>Year:</strong> ${car.year}</p>
+                    <p><strong>Make:</strong> ${car.make}</p>
+                    <p><strong>Model:</strong> ${car.model}</p>
+                    <p><strong>Price:</strong> R${car.price}</p>
+                    <button class="btn btn-remove" data-index="${index}">Remove</button>
+                `;
+                carList.appendChild(carCard);
             });
+        })
+        .catch(error => {
+            console.error('Error fetching car data:', error);
+        });
+
+    }
+    loadCarsBtn.addEventListener('click', () => {
+             loadCars();
+        
     });
-});
+
 function addCar(newCar) {
-    fetch('http://localhost:3001/cars', {
+    fetch('/api/cars', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -38,15 +44,13 @@ function addCar(newCar) {
         .then(response => response.json())
         .then(data => {
             console.log('Success:', data);
-            //reload cars
-            // const loadCarsBtn = document.getElementById('loadCarsBtn');
             loadCarsBtn.click();
         })
         .catch(error => {
             console.error('Error:', error);
         });
 }
-
+const carForm = document.getElementById('carForm');
 carForm.addEventListener('submit', event => {
     event.preventDefault();
     const make = document.getElementById('make').value;
@@ -56,19 +60,16 @@ carForm.addEventListener('submit', event => {
     addCar({ make, model, year, price });
     carForm.reset();
 });
-
 // Function to remove a car
 function removeCar(index) {
-    const carId = cars[index].id;
-    fetch(`http://localhost:3001/cars/${carId}`, {
+    console.log('Index:', index);
+    fetch('/api/cars?index='+index, {
         method: 'DELETE'
     })
         .then(response => response.json())
         .then(data => {
             console.log('Success:', data);
-            //reload cars
-            // const loadCarsBtn = document.getElementById('loadCarsBtn');
-            loadCarsBtn.click();
+            loadCars();
         })
         .catch(error => {
             console.error('Error:', error);
@@ -80,4 +81,5 @@ carList.addEventListener('click', event => {
         const index = event.target.dataset.index;
         removeCar(index);
     }
+});
 });
